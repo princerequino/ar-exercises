28468d9f5a7be2d504c5f82191825ac37d225fc8
princerequino <requino.prince@gmail.com>
2022-04-05 19:01:16 -0400

    exercise 2 and 3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -2,7 +2,16 @@ require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
 
+# 1. Load the third store (into `@store3`) as you did the other two before.
+# 2. Using Active Record's `destroy` method, delete the store from the database.
+# 3. Verify that the store has been deleted by again outputting (`puts`ing) the `count` (as you did in Exercise 1.)
+# bundle exec ruby exercises/exercise_3.rb
 puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+
+@store3 = Store.find_by(id: 3)
+@store3.destroy
+
+puts "Stores in the db: #{Store.count}"
--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -4,6 +4,12 @@ require_relative './exercise_2'
 require_relative './exercise_3'
 require_relative './exercise_4'
 
+# 1. Output the total revenue for the entire company (all stores), using Active Record's `.sum` calculation method.
+# 2. On the next line, also output the average annual revenue for all stores.
+# 3. Output the number of stores that are generating $1M or more in annual sales. **Hint:** Chain together `where` and `size` (or `count`) Active Record methods.
+
+# bundle exec ruby exercises/exercise_5.rb
+
 puts "Exercise 5"
 puts "----------"
 
